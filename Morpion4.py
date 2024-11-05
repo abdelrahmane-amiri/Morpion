81fca2ba4e7b0560f9d2f3f7b3d420cd25f641a3
Abdelrahmane-Amiri <abdelrahmane.amiri@laplateforme.io>
2024-11-05 16:36:03 +0100

    Ajout d'un partie des commentaires et detection d'un probleme(Xpeut se mettre sur O met pas l'inverse)

---
+++ Morpion4.py
@@ -0,0 +1,69 @@
+
+#Setup for game
+tableau = [[" "," "," "],[" "," "," "],[" "," "," "]]
+Player1= "X"
+Player2= "O"
+Player_Act = Player1
+row =3
+column = 3
+
+
+#function for check if a player win
+def check_Win():
+    win = False
+    if tableau[0][0] == tableau[0][1] == tableau[0][2] != " ":
+        win = True
+    if tableau[1][0] == tableau[1][1] == tableau[1][2] != " ":
+        win = True
+    if tableau[2][0] == tableau[2][1] == tableau[2][2] != " ":
+        win = True
+    if tableau[0][0] == tableau[1][0] == tableau[2][0] != " ":
+        win = True
+    if tableau[0][1] == tableau[1][1] == tableau[2][1] != " ":
+        win = True
+    if tableau[0][2] == tableau[1][2] == tableau[2][2] != " ":
+        win = True
+    if tableau[0][0] == tableau[1][1] == tableau[2][2] != " ":
+        win = True
+    if tableau[0][2] == tableau[1][1] == tableau[2][0] != " ":
+        win = True
+    return win
+    
+#function for create a board
+def creation():
+    for i in range(row):
+        print("\n+---+---+---+")
+        print("|", end="")
+        for j in range(column):
+            print("", tableau[i][j], end=" |")
+
+    print("\n+---+---+---+")
+
+#function for entering symbols
+def Saisie():
+    global Player_Act
+    click = int(input("Au " + Player_Act + " de joueur: "))
+    
+    if 1 <= click <= 9:
+            row = (click - 1) // 3
+            col = (click - 1) % 3 
+            if tableau[row][col] != Player1 and Player2:
+                tableau[row][col] = Player_Act
+
+                if Player_Act == Player1:
+                    Player_Act = Player2
+                else: Player_Act = Player1
+
+    else: print("Saisi un nombre en 1 et 9 ")
+             
+#end game
+creation()       
+while check_Win() == False: 
+    Saisie()
+    creation()
+
+if Player_Act == Player1:
+            Player_Act = Player2
+else: Player_Act = Player1
+
+print(Player_Act," a gagner")
